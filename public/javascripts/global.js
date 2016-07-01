f81449573a6e2a3b903a28c04344a370fc074f12
Taylor Jordan <Taylor.Jordan@bcg.com>
2016-07-01 15:36:02 -0400

    Readied for launch

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -11,11 +11,15 @@ function formvalidation(){
 	console.log(document.getElementById("HunterEmail").value.indexOf("@") === -1);
 	if (document.getElementById("HunterEmail").value.indexOf("@") === -1){
 		console.log("Not valid");
-		var notvalidemailnotification = document.createTextNode("Not a valid email address. (If you don't submit a valid address we won't be able to use it to contact you for the hunt");
+		var notvalidemailnotification = document.createTextNode("Not a valid email address. (If you don't submit a valid address we won't be able to use it to contact you for the hunt)");
 		//notvalidemailnotification.setAttribute("class","errortext");
 		document.getElementById("errormessageholder").appendChild(notvalidemailnotification);
 		return false;
 	}
 	
 	return true;
+}
+
+function confirmemail(email){
+	return document.getElementById("email").value === email
 }
